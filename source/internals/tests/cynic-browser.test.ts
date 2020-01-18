c40fc410ec42c783ad539e52e6edc904d3a236ec
Chase Moskal <chasemoskal@gmail.com>
2020-01-18 13:47:46 -0800

    add: manual browser test

--- source/internals/tests/cynic-browser.test.ts
+++ source/internals/tests/cynic-browser.test.ts
@@ -4,6 +4,18 @@ import {suite} from "./cynic.test.js"
 
 ;(async() => {
 	const {report, failed} = await test("cynic node tests", suite)
+
 	console.log(report)
 	console.log(failed === 0 ? "*PASSING*" : "*FAILING*")
+
+	const pre = document.createElement("pre")
+	pre.className = "report"
+	pre.textContent = report
+	document.body.appendChild(pre)
+
+	if (failed > 0) {
+		const pre2 = document.createElement("pre")
+		pre2.className = "failed"
+		document.body.appendChild(pre2)
+	}
 })()
